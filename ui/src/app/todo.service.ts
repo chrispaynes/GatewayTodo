0da1f106fbd5243ae95442d0a3ea9d63e5641e8a
chrispaynes <chrispaynes@users.noreply.github.com>
2020-05-17 18:42:45 -0600

    allow for creating a todo

--- ui/src/app/todo.service.ts
+++ ui/src/app/todo.service.ts
@@ -1,93 +1,103 @@
 import { Injectable } from '@angular/core';
 import {
-    HttpClient,
-    HttpHeaders,
-    HttpParams,
-    HttpErrorResponse,
-    HttpResponse,
+  HttpClient,
+  HttpHeaders,
+  HttpParams,
+  HttpErrorResponse,
+  HttpResponse,
 } from '@angular/common/http';
 import { Observable, of, from } from 'rxjs';
-import { map, tap, switchMap, catchError, timeout } from 'rxjs/operators';
+import {
+  map,
+  tap,
+  switchMap,
+  catchError,
+  timeout,
+  filter,
+} from 'rxjs/operators';
 import { Todo, Todos } from './todo';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class TodoService {
-    constructor(private _http: HttpClient) {}
+  constructor(private _http: HttpClient) {}
 
-    public getAllTodos(): Observable<Todo> {
-        return this._http.get('http://localhost:3000/v1/todos').pipe(
-            timeout(2000),
-            catchError((error: HttpErrorResponse) => {
-                return of(null);
-            }),
-            switchMap((x: Todos) => {
-              return x.todos
-            })
-        );
-    }
+  public getAllTodos(): Observable<Todo> {
+    return this._http.get('http://localhost:3000/v1/todos').pipe(
+      timeout(2000),
+      catchError((error: HttpErrorResponse) => {
+        return of(null);
+      }),
+      switchMap((t: Todos) => {
+        return t.todos ? t.todos : of(null);
+      })
+    );
+  }
 
-    public addTodo(todo: Todo): Observable<Todo[]> {
-        return this._http.post('http://localhost:3000/v1/todo', {}).pipe(
-            timeout(2000),
-            catchError((error: HttpErrorResponse) => {
-                return of(null);
-            }),
-            tap((x) => console.log(x)),
-            map((x: Todos) => x.todos)
-        );
-    }
+  public addTodo(todo: Todo): Observable<Todo> {
+    const body = { title: todo.title, description: todo.description };
 
-    public updateTodo(todo: Todo): Observable<Todo[]> {
-        return this._http.put('http://localhost:3000/v1/todo', {}).pipe(
-            timeout(2000),
-            catchError((error: HttpErrorResponse) => {
-                return of(null);
-            }),
-            tap((x) => console.log(x)),
-            map((x: Todos) => x.todos)
-        );
-    }
+    return this._http.post('http://localhost:3000/v1/todo', body).pipe(
+      timeout(2000),
+      catchError((error: HttpErrorResponse) => {
+        return of(null);
+      }),
+      map((t: Todo) => {
+        return t ? t : null;
+      })
+    );
+  }
 
-    public updateTodos(todos: Todos): Observable<Todo> {
-        return this._http.put('http://localhost:3000/v1/todos', todos).pipe(
-            timeout(2000),
-            catchError((error: HttpErrorResponse) => {
-                return of(null);
-            }),
-            switchMap((x: Todos) => {
-              return x.todos
-            })
-        );
-    }
+  public updateTodo(todo: Todo): Observable<Todo[]> {
+    return this._http.put('http://localhost:3000/v1/todo', {}).pipe(
+      timeout(2000),
+      catchError((error: HttpErrorResponse) => {
+        return of(null);
+      }),
+      tap((x) => console.log(x)),
+      map((x: Todos) => x.todos)
+    );
+  }
 
-    public deleteTodo(id: number): Observable<boolean> {
-        return this._http
-            .request('delete', `http://localhost:3000/v1/todo${id}`, {
-                observe: 'response',
-            })
-            .pipe(
-                timeout(2000),
-                catchError((error: HttpErrorResponse) => {
-                    return of(false);
-                }),
-                map((resp: HttpResponse<Object>) => resp && resp.status == 200)
-            );
-    }
+  public updateTodos(todos: Todos): Observable<Todo> {
+    return this._http.put('http://localhost:3000/v1/todos', todos).pipe(
+      timeout(2000),
+      catchError((error: HttpErrorResponse) => {
+        return of(null);
+      }),
+      switchMap((t: Todos) => {
+        return t.todos ? t.todos : of(null);
+      })
+    );
+  }
 
-    public deleteTodos(ids: number[]): Observable<boolean> {
-        return this._http
-            .request('delete', 'http://localhost:3000/v1/todos', {
-                observe: 'response',
-                body: { ids: ids },
-            })
-            .pipe(
-                timeout(2000),
-                catchError((error: HttpErrorResponse) => {
-                    return of(false);
-                }),
-                map((resp: HttpResponse<Object>) => resp && resp.status == 200)
-            );
-    }
+  public deleteTodo(id: number): Observable<boolean> {
+    return this._http
+      .request('delete', `http://localhost:3000/v1/todo${id}`, {
+        observe: 'response',
+      })
+      .pipe(
+        timeout(2000),
+        catchError((error: HttpErrorResponse) => {
+          return of(false);
+        }),
+        map((resp: HttpResponse<Object>) => resp && resp.status == 200)
+      );
+  }
+
+  public deleteTodos(ids: number[]): Observable<boolean> {
+    return this._http
+      .request('delete', 'http://localhost:3000/v1/todos', {
+        observe: 'response',
+        body: { ids: ids },
+      })
+      .pipe(
+        timeout(2000),
+        catchError((error: HttpErrorResponse) => {
+          return of(false);
+        }),
+        map((resp: HttpResponse<Object>) => resp && resp.status == 200)
+      );
+  }
 }
--- ui/src/app/todo/todo.component.ts
+++ ui/src/app/todo/todo.component.ts
@@ -1,100 +1,109 @@
 import { Component, OnInit, Input, HostListener } from '@angular/core';
 import {
-    FormControl,
-    FormArray,
-    FormGroup,
-    FormBuilder,
-    Validators,
+  FormControl,
+  FormArray,
+  FormGroup,
+  FormBuilder,
+  Validators,
 } from '@angular/forms';
 
 import { TodoService } from '../todo.service';
 import { Todo, TodoEdit } from '../todo';
 import { map, tap } from 'rxjs/operators';
+import { from, Observable } from 'rxjs';
 
 @Component({
-    selector: 'app-todo',
-    templateUrl: './todo.component.html',
-    styleUrls: ['./todo.component.scss'],
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss'],
 })
 export class TodoComponent implements OnInit {
-    @Input() todo: Todo;
+  @Input() todo: Todo;
 
-    @HostListener('document:keydown.escape', ['$event'])
-    onKeydownHandler(event: KeyboardEvent) {
-        console.log(event);
-        this.editMode = false;
-    }
+  @HostListener('document:keydown.escape', ['$event'])
+  onKeydownHandler(event: KeyboardEvent) {
+    console.log(event);
+    this.editMode = false;
+  }
 
-    controls: FormArray;
-    editMode: boolean = false;
-    originalTitle: string = '';
-    originalDescription: string = '';
-    form: FormGroup;
+  controls: FormArray;
+  editMode: boolean = false;
+  originalTitle: string = '';
+  originalDescription: string = '';
+  form: FormGroup;
 
-    constructor(
-        private _todoService: TodoService,
-        private formBuilder: FormBuilder
-    ) {}
+  constructor(
+    private _todoService: TodoService,
+    private formBuilder: FormBuilder
+  ) {}
 
-    ngOnInit(): void {
-        this.form = this.formBuilder.group({
-            title: [this.todo.title, Validators.required],
-            description: [this.todo.description, Validators.required],
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      title: [this.todo.title, Validators.required],
+      description: [this.todo.description, Validators.required],
+    });
+    this.originalTitle = this.todo.title;
+    this.originalDescription = this.todo.description;
+
+    this.onChanges();
+  }
+
+  onChanges(): void {
+    this.form.valueChanges.subscribe((edit: TodoEdit) => {
+      if (
+        edit.title !== this.originalTitle ||
+        edit.description !== this.originalDescription
+      ) {
+        Object.assign(this.todo, {
+          ...edit,
+          _isDirty: true,
         });
-        this.originalTitle = this.todo.title;
-        this.originalDescription = this.todo.description;
-
-        this.onChanges();
-    }
-
-    onChanges(): void {
-        this.form.valueChanges.subscribe((edit: TodoEdit) => {
-            if (
-                edit.title !== this.originalTitle ||
-                edit.description !== this.originalDescription
-            ) {
-                Object.assign(this.todo, { ...edit, _isDirty: true });
-            } else {
-                Object.assign(this.todo, { ...edit, _isDirty: false });
-            }
-
-            console.log('val', this.todo);
+      } else {
+        Object.assign(this.todo, {
+          ...edit,
+          _isDirty: false,
         });
+      }
+
+      console.log('val', this.todo);
+    });
+  }
+
+  public onFocus(event) {
+    if (!event) {
+      this.editMode = false;
     }
 
-    public onFocus(event) {
-        if (!event) {
-            this.editMode = false;
-        }
+    console.log('FOCUS', event);
+  }
 
-        console.log('FOCUS', event);
+  public onEdit(todo: Todo) {
+    this.editMode = true;
+    console.log('onEdit', todo);
+  }
+
+  public onDelete(todo: Todo) {
+    console.log('onDelete', todo);
+  }
+
+  public onAdd(todo: Todo) {
+    console.log('onAdd', todo);
+
+    if (todo.title === '' || todo.description == '') {
+      return;
     }
 
-    public onEdit(todo: Todo) {
-        this.editMode = true;
-        console.log('onEdit', todo);
-    }
+    this.reloadTodo(this._todoService.addTodo(todo));
+  }
 
-    public onDelete(todo: Todo) {
-        console.log('onDelete', todo);
-    }
+  public onUpdate(todo: Todo) {
+    console.log('onUpdate', todo);
+  }
 
-    public onAdd(todo: Todo) {
-        console.log('onAdd', todo);
-    }
-
-    public onUpdate(todo: Todo) {
-        console.log('onUpdate', todo);
-    }
-
-    public onToggleCompletion(todo: Todo) {
-        console.log('onToggleCompletion', todo);
-    }
-
-    public onSelect(event) {
-      // console.log("selected before", event);
-      // this.todo._isSelected = !event._isSelected
-      // console.log("selected after", this.todo);
-    }
-
+  // loadTodos loads todos from a datasource
+  private reloadTodo(dataSource: Observable<Todo>) {
+    from(dataSource)
+      .pipe(tap((todo: Todo) => (this.todo = todo)))
+      .subscribe();
+  }
 }
